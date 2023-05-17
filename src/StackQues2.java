e9c7b9787fce2fb2600ac9baa4e5caf059b51b27
Ut321 <64413631+Ut321@users.noreply.github.com>
2023-05-17 21:42:57 +0530

    second commit

---
+++ src/StackQues2.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class StackQues2 {
+    // Questions 2 : Reverse the String using Stack !!
+    public static void main(String[] args) {
+       String  str = "abc";
+       String result = reverseString(str);
+       System.out.println(result);
+    }
+    public  static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+        int idx=0;
+        while (idx <str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuffer result = new StringBuffer("");
+        while (!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+
+    }
+
+}
